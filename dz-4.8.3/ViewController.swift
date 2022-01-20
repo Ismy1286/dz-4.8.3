1333f4cbaeb2827d84c46ce990e939a57a7501ac
Ainur <ismy1286@gmail.com>
2022-01-20 15:23:02 +0600

    InitialCommit

--- dz-4.8.3/ViewController.swift
+++ dz-4.8.3/ViewController.swift
@@ -6,14 +6,112 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+import SnapKit
 
 
+class Cafe: Codable {
+    var cafe: [CafeModel] = []
 }
 
+class CafeModel: Codable {
+    var title: String? = nil
+    var id: Int? = nil
+    var image: String? = nil
+    var bord: [BordModel] = []
+}
+
+class BordModel: Codable {
+    var id: Int? = nil
+    var title: String? = nil
+}
+
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        
+        let json = """
+{
+  "cafe" : [
+    {
+      "title": "test1",
+      "id": 1,
+      "image": "URL",
+      "bord": [
+        {
+          "id": 1,
+          "title": "test"
+        },
+        {
+          "id": 2,
+          "title": "test"
+        }
+      ]
+    },
+    {
+      "title": "test1",
+      "id": 1,
+      "image": "URL",
+      "bord": [
+        {
+          "id": 1,
+          "title": "test"
+        },
+        {
+          "id": 2,
+          "title": "test"
+        }
+        ]
+    },
+    {
+      "title": "test1",
+      "id": 1,
+      "image": "URL",
+      "bord": [
+        {
+          "id": 1,
+          "title": "test"
+        },
+        {
+          "id": 2,
+          "title": "test"
+        }
+      ]
+    },
+    {
+      "title": "test1",
+      "id": 1,
+      "image": "URL",
+      "bord": [
+        {
+          "id": 1,
+          "title": "test"
+        },
+        {
+          "id": 2,
+          "title": "test"
+        }
+        ]
+    },
+    {
+      "title": "test1",
+      "id": 1,
+      "image": "URL",
+      "bord": [
+        {
+          "id": 1,
+          "title": "test"
+        },
+        {
+          "id": 2,
+          "title": "test"
+        }
+      ]
+    }
+  ]
+}
+"""
+
+        let model = try! JSONDecoder().decode(Cafe.self, from: Data(json.utf8))
+        dump(model)
+    }
+}
